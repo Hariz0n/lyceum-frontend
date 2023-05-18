5a22d3e6ee31a2bca7c3836c710825510ec30efa
Danila Petrov <dp200308@gmail.com>
2023-05-18 21:08:25 +0500

    add welcome component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, withDebugTracing } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { LoggedInAuthGuard } from './guards/logged-in-auth.guard';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { AuthGuard } from './guards/auth.guard';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { WelcomeGuard } from './guards/welcome.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: '',
+    pathMatch: 'full',
+    component: WelcomeComponent,
+    canActivate: [WelcomeGuard],
+  },
   {
     path: 'registration',
     component: RegistrationComponent,
@@ -23,16 +30,18 @@ const routes: Routes = [
     path: 'books',
     loadChildren: () =>
       import('./modules/books/books.module').then(m => m.BooksModule),
+    canActivate: [AuthGuard],
   },
   {
     path: 'class',
     loadChildren: () =>
       import('./modules/class/class.module').then(m => m.ClassModule),
+    canActivate: [AuthGuard],
   },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {enableTracing: true})],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
