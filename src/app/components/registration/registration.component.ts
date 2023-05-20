9c8e86145edaf00a284bfef6b51a19ea7e4dd246
Hau5gei5t <busov.artem@yandex.ru>
2023-05-20 19:54:19 +0500

    registration

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
-import {
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  Validators,
-} from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { IUser } from 'src/app/interfaces/users';
-import { LocalStorageService } from 'src/app/services/local-storage.service';
-
+import { AuthService } from '../../services/auth.service';
+interface IForm {
+  lastName: FormControl<string | null>;
+  firstName: FormControl<string | null>;
+  middleName: FormControl<string | null>;
+  email: FormControl<string | null>;
+  password: FormControl<string | null>;
+  confirmPassword: FormControl<string | null>;
+  type: FormControl<'student' | 'teacher' | null>;
+}
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
@@ -16,49 +18,87 @@ import { LocalStorageService } from 'src/app/services/local-storage.service';
 })
 export class RegistrationComponent {
   registrationForm!: FormGroup;
-  users: IUser[] = this.localStorageService.get('users') ?? [];
-  constructor(
-    private fb: FormBuilder,
-    private router: Router,
-    private localStorageService: LocalStorageService
-  ) {}
-
-  ngOnInit(): void {
-    this.registrationForm = this.fb.group({
-      login: [
-        '',
-        [Validators.required, Validators.minLength(3), this.noSpaceAllowed],
-      ],
-      password: [
-        '',
-        [Validators.required, Validators.minLength(5), this.noSpaceAllowed],
-      ],
-    });
+  constructor(private router: Router, private authService: AuthService) {
+    this.registrationForm = new FormGroup<IForm>(
+      {
+        lastName: new FormControl<string>('', [
+          Validators.required,
+          Validators.minLength(3),
+          this.noSpaceAllowed,
+        ]),
+        firstName: new FormControl<string>('', [
+          Validators.required,
+          Validators.minLength(2),
+          this.noSpaceAllowed,
+        ]),
+        middleName: new FormControl<string>('', [
+          Validators.required,
+          Validators.minLength(3),
+          this.noSpaceAllowed,
+        ]),
+        email: new FormControl<string>('', [
+          Validators.required,
+          Validators.minLength(5),
+          this.noSpaceAllowed,
+          Validators.email,
+        ]),
+        password: new FormControl<string>('', [
+          Validators.required,
+          Validators.minLength(8),
+          this.noSpaceAllowed,
+          Validators.pattern(
+            /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
+          ),
+        ]),
+        confirmPassword: new FormControl<string>('', [Validators.required]),
+        type: new FormControl<'student' | 'teacher'>('student'),
+      },
+      {
+        validators: control => {
+          if (control.value.password !== control.value.confirmPassword) {
+            control
+              .get('confirmPassword')!
+              .setErrors({ differentPasswords: true });
+          }
+          return null;
+        },
+      }
+    );
   }
 
-  get login() {
-    return this.registrationForm.get('login');
+  get lastName() {
+    return this.registrationForm.get('lastName');
   }
-
+  get firstName() {
+    return this.registrationForm.get('firstName');
+  }
+  get middleName() {
+    return this.registrationForm.get('middleName');
+  }
+  get email() {
+    return this.registrationForm.get('email');
+  }
   get password() {
     return this.registrationForm.get('password');
+  }
+
+  get confirmPassword() {
+    return this.registrationForm.get('confirmPassword');
   }
 
   async submitHandler() {
     this.registrationForm.markAllAsTouched();
     if (this.registrationForm.valid) {
-      const newUser: IUser = this.registrationForm.value;
-      if (
-        this.users.filter((user: IUser) => user.login === newUser.login).length
-      ) {
-        this.registrationForm.setErrors({ notUniqueUser: true });
-        return;
-      }
-      this.users.push(newUser);
-      this.localStorageService.save('users', this.users);
-      this.localStorageService.save('user', newUser);
-
-      this.router.navigate(['/home']);
+      console.log(this.registrationForm.value);
+      this.authService.register(this.registrationForm.value).subscribe(
+        () => {
+          this.router.navigate(['/home']);
+        },
+        e => {
+          console.log(e);
+          this.registrationForm.setErrors(['']);
+        }
+      );
     }
   }
 
@@ -68,20 +108,54 @@ export class RegistrationComponent {
     }
     return null;
   }
+
   checkPass() {
     return (
       ((this.password!.hasError('required') ||
-        this.password!.hasError('noSpaceAllowed')) &&
+        this.password!.hasError('noSpaceAllowed') ||
+        this.password!.hasError('pattern')) &&
         this.password!.touched) ||
       this.password!.hasError('minlength')
     );
   }
-  checkLogin() {
+  checkEmail() {
     return (
-      ((this.login!.hasError('required') ||
-        this.login!.hasError('noSpaceAllowed')) &&
-        this.login!.touched) ||
-      this.login!.hasError('minlength')
+      ((this.email!.hasError('required') ||
+        this.email!.hasError('noSpaceAllowed') ||
+        this.email!.hasError('email')) &&
+        this.email!.touched) ||
+      this.email!.hasError('minlength')
+    );
+  }
+  checkLastName() {
+    return (
+      ((this.lastName!.hasError('required') ||
+        this.lastName!.hasError('noSpaceAllowed')) &&
+        this.lastName!.touched) ||
+      this.lastName!.hasError('minlength')
+    );
+  }
+  checkFirstName() {
+    return (
+      ((this.firstName!.hasError('required') ||
+        this.firstName!.hasError('noSpaceAllowed')) &&
+        this.firstName!.touched) ||
+      this.firstName!.hasError('minlength')
+    );
+  }
+  checkMiddleName() {
+    return (
+      ((this.middleName!.hasError('required') ||
+        this.middleName!.hasError('noSpaceAllowed')) &&
+        this.middleName!.touched) ||
+      this.middleName!.hasError('minlength')
+    );
+  }
+  checkConfirmPassword() {
+    return (
+      (this.confirmPassword!.hasError('required') ||
+        this.confirmPassword!.hasError('differentPasswords')) &&
+      this.confirmPassword!.touched
     );
   }
 }
